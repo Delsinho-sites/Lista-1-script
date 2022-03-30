4f22c5f9fc0bac4f8d9838567dea6a759453a5da
Adelson <allanadelson6@gmail.com>
2022-03-29 21:45:48 -0300

    questão 3 e 4

---
+++ quest3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+a1=$1
+a2=$2
+
+echo -e  "Primeiro diretorio\n"
+ls ${a1}
+
+echo -e  "\nSegundo diretorio \n"
+ls ${a2}
---
+++ quest4.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+a1=$1
+a2=$2
+
+
+ls ${a1} ${a2} > /tmp/lista_linda.txt
